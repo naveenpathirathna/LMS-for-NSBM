aa4855d0ab4fe52ad0c54ec3ec916e074a51560b
Naveen Chanaka Pathirathna <naveenpathirathna@users.noreply.github.com>
2017-07-22 11:22:43 +0530

    by naveen

---
+++ Alogin.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="veiwport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="learning menagment system for nsbm">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="CSS\demo.css">
+  <link rel="stylesheet" type="text/css" href="CSS\home.css">
+</head>
+<body>
+<header>
+<!-- navgration bar -->
+		<div class="nav">
+			<ul>
+  				<li><a  class="active" href="Alogin1.php">LMS-HOME</a></li>
+  				<li><a href="AboutUS.php">About US</a></li>
+  				<li><a href="News.php">NEWS</a></li>
+  				<li><a href="Clubs.php">Clubs & socities</a></li>
+  				<li><a href="ContactUS.php">CONTACT US</a></li>        
+			</ul>
+
+		</div>
+</header>
+    <div class="img">
+      <div>
+        <h1 id="welcome2">Welcome to NSBM Learning Management System</h1>
+      </div>
+        <div class="con">
+         <div class="computing">
+             <ul>Main Menu
+             <li><a href="">NSBM Research Publication</a></li>
+             <li><a href="">Student handbook</a></li>
+             <li><a href="">Award Handbook</a></li>
+             <li><a href="">Class Time Tables</a></li>
+             <li><a href="">Exam Time Table</a></li>
+             <li><a href="">Regulation</a></li>
+        </ul>
+      </div>
+        
+        <div class="computing">
+             <ul>School of Computing
+             <li><a href="">Year 1</a></li>
+             <li><a href="">Year 2</a></li>
+             <li><a href="">Year 3</a></li>
+             <li><a href="">Year 4</a></li>
+        </ul>
+      </div>
+        
+          <div class="computing">
+             <ul>School of Buisness
+             <li><a href="">Year 1</a></li>
+             <li><a href="">Year 2</a></li>
+             <li><a href="">Year 3</a></li>
+             <li><a href="">Year 4</a></li>
+        </ul>
+      </div>
+        </div>
+    </div>
+    </body>
+</html>
---
+++ Computing.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="veiwport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="learning menagment system for nsbm">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="CSS\demo.css">
+  <link rel="stylesheet" type="text/css" href="CSS\Computing.css">
+</head>
+<body>
+<header>
+<!-- navgration bar -->
+		<div class="nav">
+			<ul>
+  				<li><a  class="active" href="Alogin1.php">LMS-HOME</a></li>
+  				<li><a href="AboutUS.php">About US</a></li>
+  				<li><a href="News.php">NEWS</a></li>
+  				<li><a href="Clubs.php">Clubs & socities</a></li>
+  				<li><a href="ContactUS.php">CONTACT US</a></li>        
+			</ul>
+
+		</div>
+</header>
+    <div class="img">
+      <div>
+        <h1 id="welcome2">School of Computing</h1>
+      </div>
+        <div class="con">
+         <div class="computing">
+             <ul>Year 1
+             <li><a href="DBMS.php">Database Management System</a></li>
+             <li><a href="">OOP with Java</a></li>
+             <li><a href="">Programming with C</a></li>
+             <li><a href="">Web Development</a></li>
+        </ul>
+      </div>
+        
+             <div class="computing">
+             <ul>Year 2
+             <li><a href="">System Analysis and Design</a></li>
+             <li><a href="">Computer Networks</a></li>
+             <li><a href="">Operating Systems</a></li>
+             <li><a href="">Mathematics</a></li>
+
+        </ul>
+      </div>        
+            <div class="computing">
+             <ul>Year 3
+             <li><a href="">Industrial Training</a></li>
+             <li><a href="">Project Management</a></li>
+             <li><a href="">Foundation of E-Bussiness</a></li>
+             <li><a href="">Final project</a></li>
+             
+        </ul>
+      </div>
+            <div class="computing">
+             <ul>Year 4
+             <li><a href="">Information Secquirty</a></li>
+             <li><a href="">Technical Communication</a></li>
+             <li><a href="">OOP iii</a></li>
+            <li><a href="">Final project</a></li>
+             
+        </ul>
+      </div>
+        </div>
+    </div>
+    </body>
+</html>
